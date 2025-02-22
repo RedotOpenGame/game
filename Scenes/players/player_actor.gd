c268ee79c83c092e68aa7c974aa8d9a6b73bc857
Pewweper <notpewweper@gmail.com>
2025-02-22 12:40:23 +0200

    added initial stuff

---
+++ Scenes/players/player_actor.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+@onready var camera = $Camera3D
+@onready var character = $characterMesh
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	var target_plane_mouse = Plane(Vector3(0, 1, 0), position.y)
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_length = 1000
+	var from = camera.project_ray_origin(mouse_pos)
+	var to = from + camera.project_ray_normal(mouse_pos) * ray_length
+	var cursor_pos_on_plane = target_plane_mouse.intersects_ray(from, to)
+	if cursor_pos_on_plane:
+		character.look_at(cursor_pos_on_plane)
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("a", "d", "w", "s")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
