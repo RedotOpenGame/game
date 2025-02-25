92d8b0b61b4db695c607af9e068fb57d33b0275a
Pewweper <notpewweper@gmail.com>
2025-02-25 14:57:28 +0200

    added following units
    they follow you in a snake-like formation.

--- Scenes/entities/NPC/follower.gd
+++ Scenes/entities/NPC/follower.gd
@@ -1,33 +1,36 @@
 extends CharacterBody3D
 
-@onready var camera = $Camera3D
+
+@onready var following:CharacterBody3D
+var follower:CharacterBody3D #meant to get who is following me so we could be in a snake-like f0rmation.
+
 @onready var character = $characterMesh
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 var interact_target:Node3D
-var follower:CharacterBody3D #meant to get who is following me so we could be in a snake-like f0rmation.
+
+func _ready() -> void:
+	following = get_tree().get_first_node_in_group("Player")
+	following.signal_follow(self)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	if is_instance_valid(following.follower) and following.follower != self:
+		following = following.follower
+		following.signal_follow(self)
 	# Handle jump.
 	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	var target_plane_mouse = Plane(Vector3(0, 1, 0), position.y)
-	var mouse_pos = get_viewport().get_mouse_position()
-	var ray_length = 1000
-	var from = camera.project_ray_origin(mouse_pos)
-	var to = from + camera.project_ray_normal(mouse_pos) * ray_length
-	var cursor_pos_on_plane = target_plane_mouse.intersects_ray(from, to)
-	if cursor_pos_on_plane:
-		character.look_at(cursor_pos_on_plane)
+
+	character.look_at(following.global_position)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("a", "d", "w", "s")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (following.global_position - position).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -38,7 +41,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	if Input.is_action_just_pressed("e") and is_instance_valid(interact_target):
 		interact_target.interaction()
-
 
 func signal_follow(body):
 	follower = body
