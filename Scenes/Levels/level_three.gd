e2fe1501286bb17da1802d7c899c23eee794fb0c
Pewweper <notpewweper@gmail.com>
2025-03-25 11:22:06 +0200

    level 3 update

---
+++ Scenes/Levels/level_three.gd
@@ -0,0 +1,19 @@
+extends Node3D
+@onready var control_point: Marker3D = $Objects/control_point
+@onready var red_wall: CyclopsBlock = $CyclopsBlocks_upgraded/RedWall
+@onready var ladder: Node3D = $CyclopsBlocks_upgraded/ladder
+
+
+func red_wall_about_to_fall() -> void: #this is a function called when one of the wall pillars die
+	if red_wall.get_child_count() == 3:
+		var tween = get_tree().create_tween()
+		tween.tween_property(red_wall, "rotation", Vector3(-PI/2, 0, 0), 1)
+		tween.tween_property(red_wall, "position", Vector3(-10, -1, -25), 1)
+
+func build_staircase() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(ladder, "position", Vector3(0, 0, 0), 1.5)
+
+
+func _on_abyss_body_entered(body: Node3D) -> void:
+	body.position = control_point.position
--- Scenes/entities/buildings/blueprint_box.gd
+++ Scenes/entities/buildings/blueprint_box.gd
@@ -6,10 +6,10 @@ extends Area3D
 
 
 var planned_bulding:PackedScene
-var unit_req:int = 3
+@export var unit_req:int = 3
 var curr_unit:int = 0
-var build_cost:int = 0 # the amount we can return
-var build_name:String = "PLACEHOLDER"
+@export var build_cost:int = 0 # the amount we can return
+@export var build_name:String = "PLACEHOLDER"
 
 func _ready() -> void:
 	planned_building.text = str("Planned building: ", build_name)
@@ -26,9 +26,10 @@ func _on_body_entered(body: Node3D) -> void:
 			constructor_req.text = str("Constructors in the area: ", curr_unit, "/", unit_req)
 		if curr_unit >= unit_req and Gameplay.scrap >= build_cost:
 			Gameplay.scrap -= build_cost
-			var scene = planned_bulding.instantiate()
-			scene.position = global_position
-			get_tree().get_first_node_in_group("AllyContainer").add_child(scene)
+			if planned_building:
+				var scene = planned_bulding.instantiate()
+				scene.position = global_position
+				get_tree().get_first_node_in_group("AllyContainer").add_child(scene)
 			queue_free()
 
 func _on_body_exited(body: Node3D) -> void:
